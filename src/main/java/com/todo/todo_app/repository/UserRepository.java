3d94470e0b09fe45f48467867a6fa1491d1889cf
karthhik09 <karthiknandeti@gmail.com>
2026-01-29 22:23:37 +0530

    added comments

--- src/main/java/com/todo/todo_app/repository/UserRepository.java
+++ src/main/java/com/todo/todo_app/repository/UserRepository.java
@@ -1,3 +1,8 @@
+/**
+ * User Repository
+ * Handles db operations for users
+ */
+
 package com.todo.todo_app.repository;
 
 import com.todo.todo_app.model.User;
@@ -5,5 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Find user by email
     Optional<User> findByEmail(String email);
 }
